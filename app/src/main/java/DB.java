445eb99ed781ce64eb78e0d88b2e8a2769dfef04
lucasramallo <lucasdebrito.jd@gmail.com>
2023-10-25 09:07:51 -0300

    Add conta em sessão ao DB

--- app/src/main/java/DB.java
+++ app/src/main/java/DB.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 
 import models.Banco;
+import models.Conta;
 
 public class DB {
     private ArrayList<Banco> bancos;
+    private Conta contaEmSessao;
 
     public DB(){
         this.bancos = new ArrayList<>();
@@ -28,5 +30,13 @@ public class DB {
 
     public void adicionarBanco(Banco novoBanco){
         this.bancos.add(novoBanco);
+    }
+
+    public void setContaEmSessao(Conta conta) {
+        this.contaEmSessao = conta;
+    }
+
+    public Conta getContaEmSessao() {
+        return this.contaEmSessao;
     }
 }
